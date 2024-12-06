ca40e71a87dc63b2f4cab84a82f3276d08a8f35d
Eduardo Hoyos <edu.hoyos92@gmail.com>
2024-12-06 13:21:16 -0500

    Value vs Reference equality

--- EquatablePlayground.playground/Contents.swift
+++ EquatablePlayground.playground/Contents.swift
@@ -26,3 +26,39 @@ let person4 = PersonA(name: "Robert", age: 22)
 
 print(person3 == person4)
 
+
+class Animal {
+
+    var name: String
+    var numberOfLegs: Int
+
+    init(name: String, numberOfLegs: Int) {
+        self.name = name
+        self.numberOfLegs = numberOfLegs
+    }
+}
+
+let animal1 = Animal(name: "Dog", numberOfLegs: 4)
+let animal2 = Animal(name: "Dog", numberOfLegs: 4)
+
+print(animal1 === animal2) // Reference Equality
+
+class AnimalA: Equatable {
+
+    var name: String
+    var numberOfLegs: Int
+
+    init(name: String, numberOfLegs: Int) {
+        self.name = name
+        self.numberOfLegs = numberOfLegs
+    }
+
+    static func == (lhs: AnimalA, rhs: AnimalA) -> Bool {
+        return lhs.name == rhs.name && lhs.numberOfLegs == rhs.numberOfLegs
+    }
+}
+
+let animal3 = AnimalA(name: "Dog", numberOfLegs: 4)
+let animal4 = AnimalA(name: "Dog", numberOfLegs: 4)
+
+print(animal3 == animal4) // Value Equality
